f6f50ba866fa7890d9ffad591edffede1fec36d2
yanyinuo <1601530253@qq.com>
2022-10-14 23:01:02 +0800

    study: java 10.14

---
+++ src/poly/poly.java
@@ -0,0 +1,2 @@
+package poly;public class poly {
+}
---
+++ src/super/_super.java
@@ -0,0 +1,12 @@
+public class _super {
+    public static void main(String[] args) {
+        /*
+        * super 代表父类的引用，用于访问构造器、方法、父类的属性
+        *
+        * 1.访问父类的属性，但不能访问父类的 private 属性 （super.属性）
+        * 2.访问父类的方法，不能访问private 方法 super.方法名
+        * 3.访问父类构造器，只能放在构造器的第一句，只能出现一句。
+        * 4. super 访问不局限于直接父类。如果多个基类都有同名成员，使用 super 访问遵循就近原则。A-> B -> C
+        **/
+    }
+}
